500dc77fa5a2d0acc2cc603f3c947a2a49257c76
Darell Kiganga <kigangadarell@gmail.com>
2023-11-22 10:00:46 +0300

    From main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 pub fn hello() {
-    let x:isize = 10;
-    let y:isize = 10;
+    let x: i64 = 10;
+    let y: i64 = 10;
     let z = x + y * 100;
     println!("{z}");
 }
 
 pub fn calle() {
     println!("Success!");
-    hello();
-}
-
-pub fn wow() {
-    println!("Great!");
     hello();
 }
