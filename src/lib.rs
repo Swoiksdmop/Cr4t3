87c22c8ffb8f042b35f8f1e525490457e8347a30
Darell Kiganga <kigangadarell@gmail.com>
2023-11-23 18:30:37 +0300

    Updated code in lib.rs, code from main.rs moved here

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
-pub fn hello() {
-    let x: i64 = 10;
-    let y: i64 = 10;
-    let z = x + y * 100;
-    println!("{z}");
-}
-
+use std::io;
 pub fn calle() {
     println!("Success!");
-    hello();
 }
+pub fn input() {
+    println!("\nWhat would you like to convert to celcius? >");
+  
+    let mut one = String::new();
+  
+    io::stdin().read_line(&mut one).expect("Failed to read line");
+  
+    let one: f64 = one.trim().parse::<f64>().expect("Failed to read line");
+    
+    let _conversion = (one - 32.0) * 5.0 / 9.0;
+  
+    println!("\n\n{one} degrees fahrenheit is {_conversion} degrees celcius");
+  }
+  
+  pub fn three_tries() {
+    for _ in 0..3 {
+      input()
+    }
+  }
